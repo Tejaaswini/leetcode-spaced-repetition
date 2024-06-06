adfba384b49ae2a2d378bb04bf83111eb2e3a8f4
tejaaswini <tejnaren07@gmail.com>
2024-06-05 21:39:41 -0400

    add table and notes section

--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
 document.getElementById('addProblemForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const problemUrl = document.getElementById('problemUrl').value;
+    const problemNotes = document.getElementById('problemNotes').value;
     if (problemUrl) {
-      addProblem(problemUrl);
+      addProblem(problemUrl, problemNotes);
       document.getElementById('problemUrl').value = '';
+      document.getElementById('problemNotes').value = '';
     }
   });
   
-  function addProblem(url) {
+  function addProblem(url, notes) {
     let problems = JSON.parse(localStorage.getItem('problems')) || [];
-    problems.push({ url: url, status: 'New', nextReview: getNextReviewDate(1) });
+    problems.push({ url: url, status: 'New', nextReview: getNextReviewDate(1), notes: notes });
     localStorage.setItem('problems', JSON.stringify(problems));
     displayProblems();
   }
@@ -25,13 +27,13 @@ document.getElementById('addProblemForm').addEventListener('submit', function (e
     const problemList = document.getElementById('problemList');
     problemList.innerHTML = '';
     problems.forEach((problem, index) => {
-      const problemElement = document.createElement('div');
+      const problemElement = document.createElement('tr');
       problemElement.innerHTML = `
-        <div>
-          ${problem.url} - ${problem.status} - Next Review: ${problem.nextReview}
-          <button onclick="editProblem(${index})" class="edit-button">Edit</button>
-          <button onclick="deleteProblem(${index})" class="delete-button">Delete</button>
-        </div>
+        <td>${problem.url}</td>
+        <td>${problem.nextReview}</td>
+        <td>${problem.notes}</td>
+        <td><button class="edit-button" onclick="editProblem(${index})">Edit</button></td>
+        <td><button class="delete-button" onclick="deleteProblem(${index})">Delete</button></td>
       `;
       problemList.appendChild(problemElement);
     });
@@ -47,11 +49,15 @@ document.getElementById('addProblemForm').addEventListener('submit', function (e
   function editProblem(index) {
     let problems = JSON.parse(localStorage.getItem('problems')) || [];
     const newUrl = prompt("Enter the new URL", problems[index].url);
+    const newNotes = prompt("Enter new notes", problems[index].notes);
     if (newUrl) {
       problems[index].url = newUrl;
-      localStorage.setItem('problems', JSON.stringify(problems));
-      displayProblems();
     }
+    if (newNotes) {
+      problems[index].notes = newNotes;
+    }
+    localStorage.setItem('problems', JSON.stringify(problems));
+    displayProblems();
   }
   
   document.addEventListener('DOMContentLoaded', displayProblems);
